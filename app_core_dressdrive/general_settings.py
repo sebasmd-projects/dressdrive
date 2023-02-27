ad2158b3d6b8335ea8889c12a0251b1bc72c2f3a
Sebastian Morales <sebasmoralesd@gmail.com>
2023-02-26 23:42:42 -0500

    change settings for media root

--- app_core_dressdrive/general_settings.py
+++ app_core_dressdrive/general_settings.py
@@ -221,7 +221,11 @@ STATICFILES_DIRS = [BASE_DIR.child('static')]
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = 'public/media/'
+
+if environment == 'local':
+    MEDIA_ROOT = BASE_DIR.child('public', 'media')
+else:
+    MEDIA_ROOT = os.getenv('MEDIA_ROOT')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
